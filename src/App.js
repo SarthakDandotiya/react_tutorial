717093bb1783b1e0817d13c50035aba6cfa104ed
Sarthak Dandotiya <sarthak.dandotiya23@gmail.com>
2018-12-26 12:25:12 +0530

    State (Add and Delete, Basic Funcitons Done)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: "Charlie",
-                job: "Janitor"
-            },
-            {
-                name: "Mac",
-                job: "Bouncer"
-            },
-            {
-                name: "Dee",
-                job: "Aspring actress"
-            },
-            {
-                name: "Dennis",
-                job: "Bartender"
-            }
-        ]
+        characters: []
     };
 
     removeCharacter = index => {
@@ -33,6 +17,10 @@ class App extends Component {
         });
     };
 
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] });
+    };
+
     render() {
         return (
             <div className='container'>
@@ -40,6 +28,7 @@ class App extends Component {
                     characterData={this.state.characters}
                     removeCharacter={this.removeCharacter}
                 />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         );
     }
