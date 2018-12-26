c55d9b5472407d43b4fab104abcd41fff25a570a
Sarthak Dandotiya <sarthak.dandotiya23@gmail.com>
2018-12-26 12:57:13 +0530

    Updates

---
+++ precache-manifest.5532819c093c9ff610f1ca29bf3fe7cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "7fd6e85e14b10e19fc40",
+    "url": "/react-tutorial/static/js/main.7fd6e85e.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "7fd6e85e14b10e19fc40",
+    "url": "/react-tutorial/static/css/main.baf72b32.chunk.css"
+  },
+  {
+    "revision": "f19f0a8b81540c9b44d6794db1607375",
+    "url": "/react-tutorial/index.html"
+  }
+];
